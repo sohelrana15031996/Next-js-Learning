e504374cf06da408cc2063eb9c0d17c3c3a1cc0d
Sohel Rana <sohelrana130523@gmail.com>
2024-10-15 21:57:25 +0100

    first commit

---
+++ app/ui/font.tsx
@@ -0,0 +1,14 @@
+
+import {Inter, Lusitana, Roboto} from 'next/font/google';
+
+export const inter = Inter({subsets:['latin'],
+  weight:['400', '700'],
+})
+export const lusitana = Lusitana ({
+  subsets:['latin'],
+  weight:['400', '700'],
+})
+export const roboto = Roboto ({
+  weight:['400', '700', '900'],
+  subsets:['latin'],
+})
